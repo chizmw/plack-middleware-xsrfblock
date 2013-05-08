9365f6283645b043338cdd3a9419506802d87f90
Chisel <chisel@chizography.net>
2013-05-08 21:39:28 +0100

    Post-minting Initial Commit

---
+++ lib/Plack/Middleware/XSRFBlock.pm
@@ -0,0 +1,8 @@
+package Plack::Middleware::XSRFBlock;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Plack::Middleware::XSRFBlock needs a more meaningful abstract
+__END__
+# vim: ts=8 sts=4 et sw=4 sr sta
